cb5c4daeaa78c0fe01c281e0d80e5a7d18981931
Jefferson Miranda <kenegocios.online@gmail.com>
2020-08-08 09:47:49 -0300

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,27 @@
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*percorrer uma coleção de todos os elementos HTML:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*procure elementos com um certo atributo:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*faça uma solicitação HTTP usando o valor do atributo como o nome do arquivo:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove o atributo, e chame essa função mais uma vez:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*sair da função:*/
+      return;
+    }
+  }
+};
